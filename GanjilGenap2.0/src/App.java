a0c6b5aa713f449a29fbb92601b8e5466c9b8ecb
SteffanoAsian <steffano.asianbudisetyo@gmail.com>
2022-04-14 11:53:59 +0700

    completed BesaeBesaran

--- GanjilGenap2.0/src/App.java
+++ GanjilGenap2.0/src/App.java
@@ -1,15 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int [] Array = new int [5];
+        int[] Array = new int[5];
         Array[0] = 4;
         Array[1] = 6;
         Array[2] = 1;
         Array[3] = 3;
         Array[4] = 5;
         System.out.print("Bilangan : ");
-        for (int element: Array){
-            System.out.print(element+" ");
+        for (int element : Array) {
+            System.out.print(element + " ");
         }
-
+        int max_num = Array[0];
+        int arr_count = 5;
+        // proses mencari nilai terbesar
+        for (int i = 0; i < arr_count; i++) {
+            if (Array[i] > max_num) {
+                max_num = Array[i];
+            }
+        }
+        System.out.println("\nAngka terbesar adalah: " + max_num);
     }
 }
